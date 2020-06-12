178d76b48d6c60553920fd0103d3a88e3c9291b6
Zacookie <blanaba1999@gmail.com>
2020-06-11 21:06:55 -0600

    README.md

---
+++ ping.js
@@ -0,0 +1,18 @@
+// The undying ping command.
+
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    config: {
+        name: "tist",
+        noalias: ""
+    },
+    run: async (bot, message) => {
+        const msg = await message.channel.send(':ping_pong: Pinging...')
+        const Embed = new MessageEmbed()
+            .setColor(3066993)
+            .setTitle('🏓 Pong!')
+            .setDescription(`Latency Ping: ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms\nAPI Ping: ${bot.ws.ping}ms`)
+        msg.edit(Embed);
+    }
+}
