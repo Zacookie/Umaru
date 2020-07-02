0accf6a497d697fcff248bf7815f9fe26b23bd80
Zacookie <blanaba1999@gmail.com>
2020-07-01 23:03:20 -0600

    poll

--- sample codes/fun/poll.js
+++ sample codes/fun/poll.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     run: async (bot, message, args) => {
         let pollChannel = message.guild.channels.cache.find(channel => channel.name === "poll");
-        if (!pollChannel) return message.channel.send("Create a channel called, `polls`. **FAST!**")
+        if (!pollChannel) return message.channel.send("Create a channel called, `poll`. **FAST!**")
       
       if (!args.join(' ')) if (!args.join(' ')) return message.channel.send("Hey kid, there's nothing to ask. Try asking something.")
       message.channel.send("I created a poll for ya. Hope you're happy about that.")
